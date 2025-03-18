1c856a0709f5c731dcd51b4312380088d7a03716
TranPhu1109 <119528426+TranPhu1109@users.noreply.github.com>
2025-03-19 04:14:07 +0700

    [Update]  Update migration

---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250318211252_UpdateV4.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateV4 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleId",
+                table: "Roles");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "RoleId",
+                table: "Roles",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
