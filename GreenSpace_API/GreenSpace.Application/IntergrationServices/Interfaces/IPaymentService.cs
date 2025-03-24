f81d87a3069b467b8d43dae00081fc04a4023345
TranPhu1109 <119528426+TranPhu1109@users.noreply.github.com>
2025-03-24 12:19:57 +0700

    [Add] add vnpay model

---
+++ GreenSpace_API/GreenSpace.Application/IntergrationServices/Interfaces/IPaymentService.cs
@@ -0,0 +1,13 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GreenSpace.Application.IntergrationServices.Interfaces
+{
+    public interface IPaymentService
+    {
+        public const string VN_PAY_BASE_URL = "https://sandbox.vnpayment.vn/paymentv2/vpcpay.html?";
+    }
+}
