c99f29e7454fca04b178fcde23cb9c1e48cabc3b
Quang1012 <135351309+Quang1012@users.noreply.github.com>
2025-03-30 12:36:52 +0700

    [update] database

--- GreenSpace_API/GreenSpace.Domain/Entities/Contract.cs
+++ GreenSpace_API/GreenSpace.Domain/Entities/Contract.cs
@@ -5,6 +5,7 @@ public class Contract : BaseEntity
     public Guid UserId { get; set; } 
 
     public string Description { get; set; } = string.Empty;
+    public Guid ServiceOrderId { get; set; }
 
     public User User { get; set; } = default!;
 }
---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250330053531_UpdateV13.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateV13 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryCode",
+                table: "ServiceOrders",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "ServiceOrderId",
+                table: "Contracts",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryCode",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropColumn(
+                name: "ServiceOrderId",
+                table: "Contracts");
+        }
+    }
+}
