b22bd9cf52fca04709f8681c2b63839a96f680e3
TranPhu1109 <119528426+TranPhu1109@users.noreply.github.com>
2025-03-31 16:10:46 +0700

    [Add] Add create bill

---
+++ GreenSpace_API/GreenSpace.WebAPI/Controllers/BillController.cs
@@ -0,0 +1,27 @@
+﻿using GreenSpace.Application.Features.Bills.Commands;
+using GreenSpace.Application.ViewModels.Bills;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace GreenSpace.WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BillController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public BillController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [ProducesResponseType((int)HttpStatusCode.OK)]
+    [ProducesResponseType((int)HttpStatusCode.InternalServerError)]
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateBillRequestModel model)
+    {
+        return Ok(await _mediator.Send(new CreateBillCommand { CreateModel = model }));
+    }
+}
