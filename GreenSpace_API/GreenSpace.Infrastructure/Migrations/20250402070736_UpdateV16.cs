f49c2836a7cca145d9f6289cce6d2307d797bd8d
Quang1012 <135351309+Quang1012@users.noreply.github.com>
2025-04-02 14:08:23 +0700

    [Update] database Complaint

---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250402070736_UpdateV16.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateV16 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Status",
+                table: "Complaints",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ComplaintType",
+                table: "Complaints",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ComplaintType",
+                table: "Complaints");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Status",
+                table: "Complaints",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+    }
+}
