dcdd5efcbb7bc12a8f8b3a334b7f9e10a0e1e493
TranPhu1109 <119528426+TranPhu1109@users.noreply.github.com>
2025-04-11 20:04:09 +0700

    [Update] update database

---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250411130328_UpdateV20.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateV20 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ComplaintDetail",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ComplaintId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    CreationDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    ModificationDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModificatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ComplaintDetail", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ComplaintDetail_Complaints_ComplaintId",
+                        column: x => x.ComplaintId,
+                        principalTable: "Complaints",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_ComplaintDetail_Materials_ProductId",
+                        column: x => x.ProductId,
+                        principalTable: "Materials",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComplaintDetail_ComplaintId",
+                table: "ComplaintDetail",
+                column: "ComplaintId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ComplaintDetail_ProductId",
+                table: "ComplaintDetail",
+                column: "ProductId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ComplaintDetail");
+        }
+    }
+}
