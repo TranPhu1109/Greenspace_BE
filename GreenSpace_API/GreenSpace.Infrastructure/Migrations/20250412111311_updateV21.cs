61a7a86a34d9c40aadf58ef1b088476a50b6e6c1
Quang1012 <135351309+Quang1012@users.noreply.github.com>
2025-04-12 18:14:07 +0700

    [Update] database

---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250412111311_updateV21.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateV21 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "DeliveryCode",
+                table: "Complaints",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "ComplaintDetail",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "ComplaintDetail",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryCode",
+                table: "Complaints");
+
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "ComplaintDetail");
+
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "ComplaintDetail");
+        }
+    }
+}
