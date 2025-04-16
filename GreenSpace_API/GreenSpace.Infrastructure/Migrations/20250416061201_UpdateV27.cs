cf8001060286bcdafb9dd885ed90ef6dc4d5e277
Quang1012 <135351309+Quang1012@users.noreply.github.com>
2025-04-16 14:53:07 +0700

    [Update] database

---
+++ GreenSpace_API/GreenSpace.Infrastructure/Migrations/20250416061201_UpdateV27.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GreenSpace.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateV27 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DepositPercentage",
+                table: "ServiceOrders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "RefundPercentage",
+                table: "ServiceOrders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DepositPercentage",
+                table: "ServiceOrders");
+
+            migrationBuilder.DropColumn(
+                name: "RefundPercentage",
+                table: "ServiceOrders");
+        }
+    }
+}
